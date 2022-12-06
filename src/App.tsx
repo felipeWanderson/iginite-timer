21ff06a6e0f2bd520f1ba9e62e76991ebf7e6d2b
Felipe Leal <felipe.leal@compasso.com.br>
2022-12-06 00:04:03 -0300

    feat: create pages e add rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from 'styled-components'
+
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/defaults'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Ignite Timer</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
